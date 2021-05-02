4923e478f6a7a1fdb769c1a436ab4672424290c9
unknown <rak25.mirza@gmail.com>
2021-05-02 04:17:56 -0400

    add new files in template and product folder

--- djago_portfolio/urls.py
+++ djago_portfolio/urls.py
@@ -17,13 +17,16 @@ from django.contrib import admin
 from django.urls import path
 #from pages import views
 from pages.views import home_view, contact_view,about_view
+from product.views import product_detail_view
 
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('contact/',contact_view),
-    path('about/', about_view)
+    path('about/', about_view),
+   # path('admin/'admin.site.urls),
+    path('product/',product_detail_view)
     
     
 ]
--- pages/views.py
+++ pages/views.py
@@ -9,11 +9,17 @@ def home_view(request, *args, **kwargs):
     return  render(request,"home.html",{}) # string of HTML code 
 
 def contact_view(request, *args, **kwargs):
-    return render(request,"contact.html" )
+    return render(request,"contact.html",{} )
 
 
 def about_view(request ,*args, **kwargs):
-    return render(request,'about.html')
+    my_context = {
+        "my_text" : "This is about me",
+        "my _number" :  123,
+        "this_is_true" :True,
+        "my_list" : [123, 4243, 12312, 321,"ABC"]
+    }
+    return render(request,'about.html',my_context)
 
 
 
